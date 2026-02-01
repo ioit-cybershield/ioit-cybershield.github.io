336a0d78de27e9aa9199414f464f2c20b51f32a7
Meghraj Jare <meghrajjare77@gmail.com>
2026-02-01 21:45:21 +0530

    events: redo

--- src/content/calender.ts
+++
@@ -1,8 +0,0 @@
-const events = [
-  {
-    date: new Date(2026, 1, 2),
-    id: "linear",
-    icon: "https://upload.wikimedia.org/wikipedia/commons/9/97/Linear_logo.svg",
-    color: "#5E6AD2",
-  },
-];
